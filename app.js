9fe4efeedfd8e00199d8d19f260dd29bcfeb0c79
Your Name <you@example.com>
2024-09-13 13:12:36 +0800

    initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+let name = "Denver Valentino";
+console.log ('My Name ' + name);
+
+let whatDoYouWannaBecomeInYourLife ="programmer";
+console.log ('I Will Be a ' + whatDoYouWannaBecomeInYourLife);
+
+let gender = "Male";
+console.log (gender);
+
+let Twitterhandle = "@Denver_G18";
+console.log (Twitterhandle);
